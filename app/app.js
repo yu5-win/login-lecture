27c649185f73dabebe03084b14b03ec4573bbe6e
yu5-win <yuseng1021@gmail.com>
2021-08-20 15:23:14 +0900

    userstorage

--- app/app.js
+++ app/app.js
@@ -2,8 +2,8 @@
 
 // 모듈
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
-
 
 // 라우팅
 const home = require('./src/routes/home');
@@ -11,6 +11,9 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set("views", "./src/views");
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use('/', home); // use -> 미들 웨어를 등록해주는 메서드
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,39 @@
 "use strict";
 
-const hello = (req, res) => {
-    res.render('home/index');
-};
+const UserStorage = require("../../models/UserStorage");
 
-const login = (req, res) => {
-    res.render('home/login');
+const output = {
+    hello: (req, res) => {
+        res.render('home/index');
+    },
+    
+    login: (req, res) => {
+        res.render('home/login');
+    },
+}
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id;
+        const psword = req.body.psword;
+        
+        const users = UserStorage.getUsers("id", "psword");
+
+        const response = {};
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                response.success = true;
+                return res.json(response);
+            }
+        }
+        response.success = false;
+        response.msg = "로그인에 실패하셨습니다.";
+        return res.json(response);
+    },
 };
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,
 };
